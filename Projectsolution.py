dce429e4c66064c65f274302820fbf31fd2c920f
bensontjohn <bensontjohn@gmail.com>
2019-04-04 20:07:28 +0100

    First draf of Python file with reading dataset and print the data

---
+++ Projectsolution.py
@@ -0,0 +1,9 @@
+# Benson Thomas John, 2019
+# Project entailing researching Fisher's Iris data set
+
+# import pandas library module
+import pandas
+
+dataset = pandas.read_csv('iris.csv')
+
+print(dataset)
