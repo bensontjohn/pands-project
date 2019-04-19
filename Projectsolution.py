4aa5b24ec86ca1b649fa9824e3b5c30cce700c38
Benson Thomas John <bensontjohn@gmail.com>
2019-04-19 19:31:45 +0100

    Added comments for projectsolution.py

--- Projectsolution.py
+++ Projectsolution.py
@@ -3,8 +3,11 @@
 
 # import pandas library module
 import pandas
+
+#import matplotlib.pyplot
 import matplotlib.pyplot as plt
 
+# read the csv dataset and store in dataset variable
 dataset = pandas.read_csv('iris.csv')
 
 # To print the first 5 rows from the dataset
@@ -14,5 +17,8 @@ print(dataset.head())
 # To find the number of rows and columns in the dataset
 print(dataset.shape)
 
+# create histogram of the dataset
 dataset.hist()
+
+# displays the histogram plot
 plt.show()
