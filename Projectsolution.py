6d4eb9c2bc124cddb7169abbf02e16c131b213f4
bensontjohn <bensontjohn@gmail.com>
2019-04-22 20:57:20 +0100

    Find unique, min, max, mean of each column of the dataset

--- Projectsolution.py
+++ Projectsolution.py
@@ -10,16 +10,26 @@ import matplotlib.pyplot as plt
 import numpy
 
 df = pd.read_csv (r'iris.csv')
-# print(df)
+
 # read the csv dataset and store in dataset variable
 dataset = pd.read_csv('iris.csv')
 
-# Adapted from https://datatofish.com/use-pandas-to-calculate-stats-from-an-imported-csv-file/
-mean1 = df['sepal_length'].mean()
-print("Mean of the sepal length of the 3 species: " +str(mean1))
+# Adapted from https://github.com/RitRa/Project2018-iris
+
+# Print the unique species
+unique = dataset['species'].unique()
+print(unique)
+
+# Print the minimum values for each of the attributes
+print(dataset.min())
+
+# Print the maximun values for each of the attributes
+print(dataset.max())
+
+# Print the mean values for each of the attributes
+print(dataset.mean())
 
 # To print the first 5 rows from the dataset
-
 print(dataset.head())
 
 # To find the number of rows and columns in the dataset
