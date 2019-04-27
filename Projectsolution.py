34eebba9483c599771f8a01a9b77f28b035779c5
bensontjohn <bensontjohn@gmail.com>
2019-04-27 18:54:55 +0100

    Added describe, transpose methods in projectsolution.py file

--- Projectsolution.py
+++ Projectsolution.py
@@ -1,5 +1,5 @@
 # Benson Thomas John, 2019
-# Project entailing researching Fisher's Iris data set
+# Project researching Fisher's Iris data set
 
 # import pandas library module
 import pandas as pd
@@ -10,17 +10,23 @@ import matplotlib.pyplot as plt
 # read the csv dataset and store in dataset variable
 dataset = pd.read_csv('iris.csv')
 
-# Adapted from https://github.com/RitRa/Project2018-iris
+# Referenced:  https://github.com/RitRa/Project2018-iris
 
 # Print the unique species
 unique = dataset['species'].unique()
 print(unique)
 
+# Referenced: http://www.datasciencemadesimple.com/get-minimum-value-column-python-pandas/
+
 # Print the minimum values for each of the attributes
 print(dataset.min())
 
-# Print the maximun values for each of the attributes
+# Referenced: https://www.geeksforgeeks.org/python-pandas-dataframe-max/
+
+# Print the maximum values for each of the attributes
 print(dataset.max())
+
+# Referenced: https://www.geeksforgeeks.org/python-pandas-dataframe-mean/
 
 # Print the mean values for each of the attributes
 print(dataset.mean())
@@ -30,6 +36,17 @@ print(dataset.head())
 
 # To find the number of rows and columns in the dataset
 print(dataset.shape)
+
+# Adapted from https://stackoverflow.com/a/38863443
+
+# To view statistical details like percentile, count, mean, standard deviation
+summary = dataset.describe()
+
+# To write rows as columns and vice-versa
+summary = summary.transpose()
+
+# Print the first 5 rows of the summary variable values from the dataset
+print(summary.head())
 
 # create histogram of the dataset
 dataset.hist()
